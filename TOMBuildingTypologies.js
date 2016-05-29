0c82c3719e1b8c8f1ee9ebec39bfd6f51f90e037
Reinier Battenberg <reinier@mountbatten.nl>
2016-05-29 22:48:26 +0200

    Updated functionality and proper styling

--- TOMBuildingTypologies.js
+++ TOMBuildingTypologies.js
@@ -1,30 +1,21 @@
 "use strict";
-define(
-  ['require', 'scene'],
-  function(require, Scene) {
+define(['require', 'scene'], function(require, Scene) {
 
-    class SceneTOMBuildingTypologies extends Scene {
-      constructor() {
-          console.log("Going to Super Scene TOMBuildingTypologies");
-          var config = {
-            "id": "TOMBuildingTypologies",
-            "groupid": "Urban",
-            "languages": ['en-US', 'nl-NL'],
-          };
-          super(config);
-        }
-        // You must copy this, else we do not know where we are
-      get path() {
-        return require.toUrl("./").split('?')[0];
+  class SceneTOMBuildingTypologies extends Scene {
+    constructor() {
+        console.log("Going to Super Scene TOMBuildingTypologies");
+        var config = {
+          "id": "TOMBuildingTypologies",
+          "groupid": "Urban",
+          "languages": ['en-US', 'nl-NL'],
+        };
+        super(config);
       }
-      get legend() {
-        //  var startdates = getStartDateArray();
-        //var years = Object.keys(startdates);
-        var legend = [];
-        legend['items'] = [];
-        return legend;
-      };
-    };
-    return new SceneTOMBuildingTypologies();
+      // You must copy this, else we do not know where we are
+    get path() {
+      return require.toUrl("./").split('?')[0];
+    }
+  };
+  return new SceneTOMBuildingTypologies();
 
-  });
+});
